dd2b382409a084f185feaaf3a27b29d48afec5a5
MatAviez05 <baezavilamateo@gmail.com>
2025-09-29 10:40:31 -0300

    practico 2 - estructura

---
+++ Practico 2/readme.md
@@ -0,0 +1,11 @@
+# Practico 2 - Programacion IV
+
+## Ejecutar
+```bash
+npm i
+```
+## Integrantes del grupo
++ Mateo Avila Baez
++ Ramiro Gabeiras
++ Gino Robla Belleggia
++ Lautaro Carrio
---
+++ Practico 2/src/app.ts
@@ -0,0 +1,27 @@
+import express from 'express';
+import cors from 'cors';
+
+class Server {
+    public app: express.Application;
+    public port: number;
+
+    constructor(port: number) {
+        this.port = port;
+        this.app = express();
+        this.middlewares();
+        this.routes();
+        
+    }
+    middlewares(){
+        this.app.use(express.json({limit: '150mb'}));
+        //cors
+        this.app.use( cors());
+    }
+    routes(){
+        // this.app.use("/products",productRouote)
+    }
+    start(callback: () => void) {
+        this.app.listen(this.port, callback);
+    }
+}
+export default Server;
