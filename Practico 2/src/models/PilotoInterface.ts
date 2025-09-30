3621cc770f276b9febeab7041d867f6347e63cec
MatAviez05 <baezavilamateo@gmail.com>
2025-09-30 17:22:47 -0300

    interface pilotos y service sin implmentar las funciones

---
+++ Practico 2/src/models/PilotoInterface.ts
@@ -0,0 +1,9 @@
+export type category = "TC" | "TCP" | "TCPK" | "TCPPK" | "TN" | "TP" | "TC2000"
+
+export default interface Piloto {
+    id:number
+    nombre:string,
+    marca:string,
+    categoria:category,
+    activo:boolean
+}
---
+++ Practico 2/src/service/ACTCservice.ts
@@ -0,0 +1,33 @@
+import Piloto from "../models/PilotoInterface";
+import { category } from "../models/PilotoInterface";
+
+export default class ACTC {
+
+    private pilotosList: Piloto[] = []
+    private id_autoincremental: number = 1
+
+    //funcion solo para chequear la lista
+    public getPilotos(): Piloto[] {
+        return this.pilotosList
+    }
+
+    public getPilotoById(idF: number): Piloto | undefined {
+        throw new Error('Funcion no implmentada')
+    }
+
+    public getPilotoByCategory(categ: category): Piloto[] | []{
+        throw new Error('Funcion no implmentada')
+    }
+    //parametros: id uso 'id_autoincremental' y activo siempre 'true' cuando se crea
+    public addPiloto(nombre: string, marca: string, categoria: category): Piloto {
+        throw new Error('Funcion no implmentada')
+    }
+
+    public deletePiloto(idF: number): boolean {
+        throw new Error('Funcion no implmentada')
+    }
+
+    public setActivo(idF: number, activo:boolean ): boolean {
+        throw new Error('Funcion no implmentada')
+    }
+}
