fcc4c722ac8d6c1c3ea6775fc82a99354a4b65bf
MatAviez05 <baezavilamateo@gmail.com>
2025-10-01 21:35:15 -0300

    feat: US2 - integration GET /actc/pilotos/cat?categoria="" (rojo)

---
+++ Practico 2/tests/integration/ACTCservice.getPilotoByCategory.integration.test.ts
@@ -0,0 +1,36 @@
+import request from "supertest"
+import Server from "../../src/app"
+import { describe, it, expect } from "vitest"
+
+describe('GET /actc/pilotos/cat?categoria="" - validaciones', () => {
+    const server = new Server(3000)
+    const app = server.app
+
+    it('GET /actc/pilotos/cat?categoria=TC - ok (200)', async () => {
+        await request(app).post('/actc/pilotos').send({
+            nombre: 'Mariano Werner',
+            marca: 'Toyota',
+            categoria: 'TCPK'
+        })
+        await request(app).post('/actc/pilotos').send({
+            nombre: 'Gaston Mazzacane',
+            marca: 'Toyota',
+            categoria: 'TCPK'
+        })
+        await request(app).post('/actc/pilotos').send({
+            nombre: 'German Todino',
+            marca: 'Ford',
+            categoria: 'TCPK'
+        })
+        await request(app).post('/actc/pilotos').send({
+            nombre: 'Agustin Canapino',
+            marca: 'Renault',
+            categoria: 'TC2000'
+        })
+
+        const get = await request(app)
+        .get('/actc/pilotos/cat?categoria=TCPK')
+
+        expect(get.status).toBe(200)
+    })
+})
