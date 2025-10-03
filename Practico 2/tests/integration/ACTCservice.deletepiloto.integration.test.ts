7bb3eb448ab504a4fdb725fe2545a0d0b6cbb504
MatAviez05 <baezavilamateo@gmail.com>
2025-10-03 20:16:36 -0300

    feat: US4 - integration DELETE /actc/pilotos/:id (rojo)

---
+++ Practico 2/tests/integration/ACTCservice.deletepiloto.integration.test.ts
@@ -0,0 +1,56 @@
+import request from "supertest"
+import Server from "../../src/app"
+import { describe, it, expect } from "vitest"
+
+describe('DELETE /actc/pilotos/:id - validaciones', () => {
+    const server = new Server(3000)
+    const app = server.app
+
+    it('DELETE /actc/pilotos/:id - ok (204)', async () => {
+        await request(app).post('/actc/pilotos').send({
+            nombre: 'Gaston Mazzacane',
+            marca: 'Toyota',
+            categoria: 'TCPK'
+        })
+        await request(app).post('/actc/pilotos').send({
+            nombre: 'German Todino',
+            marca: 'Ford',
+            categoria: 'TCPK'
+        })
+        await request(app).post('/actc/pilotos').send({
+            nombre: 'Agustin Canapino',
+            marca: 'Renault',
+            categoria: 'TC2000'
+        })
+
+        const id:number = 1
+        const delt = await request(app)
+        .delete(`/actc/pilotos/${id}`)
+        expect(delt.status).toBe(204)
+        expect(delt.body).toHaveProperty('message')
+    })
+
+    it('DELETE /actc/pilotos/:id - error (404)', async () => {
+        await request(app).post('/actc/pilotos').send({
+            nombre: 'Gaston Mazzacane',
+            marca: 'Toyota',
+            categoria: 'TCPK'
+        })
+        await request(app).post('/actc/pilotos').send({
+            nombre: 'German Todino',
+            marca: 'Ford',
+            categoria: 'TCPK'
+        })
+        await request(app).post('/actc/pilotos').send({
+            nombre: 'Agustin Canapino',
+            marca: 'Renault',
+            categoria: 'TC2000'
+        })
+
+        const id:number = 9
+        const delt = await request(app)
+        .delete(`/actc/pilotos/${id}`)
+        expect(delt.status).toBe(404)
+        expect(delt.body).toHaveProperty('error')
+    })
+})
