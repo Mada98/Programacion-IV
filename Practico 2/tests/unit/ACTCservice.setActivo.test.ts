15ac2a08a65c4435c076a268bba126f20b09567d
MatAviez05 <baezavilamateo@gmail.com>
2025-10-04 12:44:08 -0300

    test: US5 - modificar el estado del piloto por ID (rojo)

---
+++ Practico 2/tests/unit/ACTCservice.setActivo.test.ts
@@ -0,0 +1,11 @@
+import {describe, it, expect} from 'vitest'
+
+import ACTC from '../../src/service/ACTCservice'
+
+describe('US4 - ACTCservice - setActivo function', () => {
+    const svc = new ACTC()
+    const piloto1 = svc.addPiloto('Julian', 'Ford', 'TC') //crea el piloto con estado activo=true
+    const succes = svc.setActivo(1, false)
+    expect(succes).toBe(true)
+    expect(svc.getPilotoById(1)?.activo).toBe(false)
+})
