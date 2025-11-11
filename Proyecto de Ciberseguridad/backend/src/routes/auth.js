7cdb01eb907ec722a2c2d3e11e5a759d596a6d8b
MatAviez05 <baezavilamateo@gmail.com>
2025-11-10 21:46:39 -0300

    Fuerza bruta: 1 - El endpoint de login debe tener protección contra fuerza bruta

--- Proyecto de Ciberseguridad/backend/src/routes/auth.js
+++ Proyecto de Ciberseguridad/backend/src/routes/auth.js
@@ -1,9 +1,18 @@
 const express = require('express');
 const router = express.Router();
 const authController = require('../controllers/authController');
+const rateLimit = require('express-rate-limit')
+
+const loginLimiter = rateLimit({
+  windowMs: 15 * 60 * 1000,
+  max: 5,
+  handler: (req, res) => {
+    res.status(429).send('Demasiados intentos. Inténtalo más tarde.');
+  }
+});
 
 // Rutas de autenticación
-router.post('/login', authController.login);
+router.post('/login', loginLimiter, authController.login);
 router.post('/register', authController.register);
 router.post('/auth/verify', authController.verifyToken);
 router.post('/check-username', authController.checkUsername);
