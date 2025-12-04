c1e4c42ee0615ccb224f0f2d4b4956324d870d04
MatAviez05 <baezavilamateo@gmail.com>
2025-12-04 14:07:21 -0300

    update jest.config

--- Proyecto de Ciberseguridad/backend/jest.config.js
+++ Proyecto de Ciberseguridad/backend/jest.config.js
@@ -7,15 +7,8 @@ module.exports = {
     'src/middleware/**/*.js',
     '!**/node_modules/**'
   ],
-  testPathIgnorePatterns: ['/node_modules/', '05-file-upload'],
   testMatch: [
-    '**/test/**/01-*.test.js',
-    '**/test/**/02-*.test.js',
-    '**/test/**/03-*.test.js',
-    '**/test/**/04-*.test.js',
-    '**/test/**/06-*.test.js',
-    '**/test/**/07-*.test.js',
-    '**/test/**/08-*.test.js'
+    '**/test/**/*.test.js'
   ],
   verbose: true,
   testTimeout: 10000,
